9c45f4bc2e450a04426462687abaf7d05af2cc04
wiphoo <wiphoo.m@gmail.com>
2018-04-28 21:15:04 +0700

    fixed the method for waiting the response after requested

--- Calling_Web_Service_using_SOAP_Request/Program.cs
+++ Calling_Web_Service_using_SOAP_Request/Program.cs
@@ -9,8 +9,10 @@ using System.Xml;
 
 //  assert
 using System.Diagnostics;
+using System.Xml.Linq;
 
-//  ref - https://www.c-sharpcorner.com/article/calling-web-service-using-soap-request/
+//  ref -   https://www.c-sharpcorner.com/article/calling-web-service-using-soap-request/
+//          https://stackoverflow.com/questions/4791794/client-to-send-soap-request-and-received-response?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 
 namespace Calling_Web_Service_using_SOAP_Request
 {
@@ -19,21 +21,22 @@ namespace Calling_Web_Service_using_SOAP_Request
         static void Main(string[] args)
         {
             //creating object of program class to access methods    
-            Program obj = new Program();
-            Console.WriteLine("Please Enter Input values..");
-            //Reading input values from console
-            
-            //  Year
-            Console.Write("Enter from year : ");
-            int fromYear = Convert.ToInt32(Console.ReadLine());
-            Console.Write("      to year : ");
-            int toYear = Convert.ToInt32(Console.ReadLine());
-            
-            //  Month
-            Console.Write("Enter from month : ");
-            int fromMonth = Convert.ToInt32(Console.ReadLine());
-            Console.Write("      to month : ");
-            int toMonth = Convert.ToInt32(Console.ReadLine());
+            //Program obj = new Program();
+
+            //Console.WriteLine("Please Enter Input values..");
+            ////Reading input values from console
+
+            ////  Year
+            //Console.Write("Enter from year : ");
+            //int fromYear = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("      to year : ");
+            //int toYear = Convert.ToInt32(Console.ReadLine());
+
+            ////  Month
+            //Console.Write("Enter from month : ");
+            //int fromMonth = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("      to month : ");
+            //int toMonth = Convert.ToInt32(Console.ReadLine());
 
 #warning UNCOMMENT_ME
             ////  harmonize code
@@ -61,45 +64,126 @@ namespace Calling_Web_Service_using_SOAP_Request
             //    }
             //}
 
-#warning REMOVE_ME :: TESTING PURPOSE
-            //  harmonize code
-            Console.Write("Enter harmonize code : ");
-            string harmonizeCodeStr = Convert.ToString(Console.ReadLine());
-            List<string> harmonizeCodeList = new List<string>();
-            harmonizeCodeList.Add( harmonizeCodeStr );
+            //#warning REMOVE_ME :: TESTING PURPOSE
+            //            //  harmonize code
+            //            Console.Write("Enter harmonize code : ");
+            //            string harmonizeCodeStr = Convert.ToString(Console.ReadLine());
+            //            List<string> harmonizeCodeList = new List<string>();
+            //            harmonizeCodeList.Add( harmonizeCodeStr );
 
-            //  rank
-            Console.Write("Enter from rank : ");
-            int fromRank = Convert.ToInt32(Console.ReadLine());
-            Console.Write("      to rank : ");
-            int toRank = Convert.ToInt32(Console.ReadLine());
+            //            //  rank
+            //            //Console.Write("Enter from rank : ");
+            //            //int fromRank = Convert.ToInt32(Console.ReadLine());
+            //            //Console.Write("      to rank : ");
+            //            //int toRank = Convert.ToInt32(Console.ReadLine());
+            //            Console.Write( "Enter number of rank : " );
+            //            int numRanks = Convert.ToInt32(Console.ReadLine());
 
-            //  loop over all year/month/rank and call the service
-            for( int year = fromYear ; year <= toYear ; ++ year )
-            {
-                for( int month = fromMonth ; month <= toMonth ; ++ month )
-                {
-                    foreach( string harmonizeCode in harmonizeCodeList )
-                    {
-                        for( int rank = fromRank ; rank <= toRank ; ++ rank )
-                        {
-                            //  Calling InvokeService method    
-                            obj.InvokeServiceExportHarmonizeCountry( year, month, rank, harmonizeCode );
-                        }
-                    }
-                }
-            }
+            //            //  loop over all year/month/rank and call the service
+            //            for ( int year = fromYear ; year <= toYear ; ++ year )
+            //            {
+            //                for( int month = fromMonth ; month <= toMonth ; ++ month )
+            //                {
+            //                    foreach( string harmonizeCode in harmonizeCodeList )
+            //                    {
+            //                        //  Calling InvokeService method    
+            //                        obj.InvokeServiceExportHarmonizeCountry( year, month, numRanks, harmonizeCode );
+            //                    }
+            //                }
+            //            }
+
+            invokeServiceImportHarmonizeCountry( 2017, 8, 99999, "390410" );
+            Console.WriteLine( "--------------------------------------------------" );
+
+            string urlGetExportHamonizeCountry = @"http://www2.ops3.moc.go.th/tradeWebservice/ServiceExportHarmonizeCountry.asmx",
+                    actionGetExportHamonizeCountry = "http://tempuri.org/GetExportHarmonizeCountry";
+
+            HttpWebRequest requestGetExportHamonizeCountry = createSOAPWebRequest( urlGetExportHamonizeCountry, actionGetExportHamonizeCountry );
+            XmlDocument envelopeGetExportHamonizeCountry = createSOAPEnvelopeForGetExportHarmonizeCountry( 2017, 8, "390410", 10 );
+            callWebService( requestGetExportHamonizeCountry, envelopeGetExportHamonizeCountry );
 
             Console.ReadLine();
         }
 
-        public HttpWebRequest CreateSOAPWebRequestForExportHarmonizeCountry()
+        public static void callWebService( HttpWebRequest request, XmlDocument envelope )
+        {
+            //  insert the envelope into web request
+            using ( Stream stream = request.GetRequestStream() )
+            {
+                envelope.Save( stream );
+            }
+
+            // begin async call to web request.
+            IAsyncResult asyncResult = request.BeginGetResponse( null, null );
+
+            // suspend this thread until call is complete. You might want to
+            // do something usefull here like update your UI.
+            asyncResult.AsyncWaitHandle.WaitOne();
+
+            // get the response from the completed web request.
+            string soapResponse;
+            using ( WebResponse webResponse = request.EndGetResponse( asyncResult ) )
+            {
+                using ( StreamReader streamReader = new StreamReader( webResponse.GetResponseStream() ) )
+                {
+                    soapResponse = streamReader.ReadToEnd();
+                }
+
+                //  parse response
+                XElement response = XElement.Parse( soapResponse );
+
+                //  writting stream result on console
+                Console.WriteLine( response );
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------
+        //  GetExportHarmonizeCountry
+
+        public static HttpWebRequest createSOAPWebRequest( string url, string action )
+        {
+            //  create the http web request header
+            HttpWebRequest webRequest = ( HttpWebRequest )WebRequest.Create( url );
+            webRequest.Headers.Add( "SOAPAction", action );
+            webRequest.ContentType = "text/xml; charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+       
+        public static XmlDocument createSOAPEnvelopeForGetExportHarmonizeCountry( int year, int month, string harmonizeCode, int numRanks )
+        {
+            Console.WriteLine( String.Format( "createSOAPEnvelopeForGetExportHarmonizeCountry : ( year = {0}, month = {1}, rank = {2}, harmonize code = {3} )", year.ToString(), month.ToString(), harmonizeCode, numRanks.ToString()  ) );
+            //  create empty SOAP envelope document
+            XmlDocument soapEnvelopeDocument = new XmlDocument();
+
+            //  set the SOAP envelope
+            soapEnvelopeDocument.LoadXml( @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+    <soap12:Envelope xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:soap12=""http://www.w3.org/2003/05/soap-envelope"">
+        <soap12:Body>
+            <GetExportHarmonizeCountry xmlns=""http://tempuri.org/"">
+                <Yearno>" + year + @"</Yearno>
+                <Monthno>" + month + @"</Monthno>
+                <HarmonizeCode>" + harmonizeCode + @"</HarmonizeCode>
+                <NoRank>" + numRanks + @" </NoRank>
+            </GetExportHarmonizeCountry>
+        </soap12:Body>
+    </soap12:Envelope>" );
+
+            //  return xml soap document
+            return soapEnvelopeDocument;
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------
+        //  GetImportHarmonizeCountry
+
+        public static HttpWebRequest createSOAPWebRequestForImportHarmonizeCountry()
         {
             //  making Web Request    
             HttpWebRequest httpWebRequest = ( HttpWebRequest )WebRequest.Create( @"http://www2.ops3.moc.go.th/tradeWebservice/ServiceImportHarmonizeCountry.asmx" );
             //SOAPAction    
             httpWebRequest.Headers.Add( @"SOAPAction:http://tempuri.org/GetImportHarmonizeCountry" );
-            //httpWebRequest.Host = "www2.ops3.moc.go.th";
+            httpWebRequest.Host = "www2.ops3.moc.go.th";
             //Content_type    
             httpWebRequest.ContentType = "text/xml;charset=\"utf-8\"";
             httpWebRequest.Accept = "text/xml";
@@ -109,11 +193,11 @@ namespace Calling_Web_Service_using_SOAP_Request
             return httpWebRequest;
         }
 
-        public void InvokeServiceExportHarmonizeCountry( int year, int month, int rank, string harmonizeCode )
+        public static void invokeServiceImportHarmonizeCountry( int year, int month, int rank, string harmonizeCode )
         {
             Console.WriteLine( String.Format("Getting the export harmonize country : ( year = {0}, month = {1}, rank = {2}, harmonize code = {3} )", year.ToString(), month.ToString(), rank.ToString(), harmonizeCode ) );
             //Calling CreateSOAPWebRequest method
-            HttpWebRequest request = CreateSOAPWebRequestForExportHarmonizeCountry();
+            HttpWebRequest request = createSOAPWebRequestForImportHarmonizeCountry();
 
             XmlDocument SOAPReqBody = new XmlDocument();
             SOAPReqBody.LoadXml(@"<?xml version=""1.0"" encoding=""utf-8""?>
@@ -123,13 +207,13 @@ namespace Calling_Web_Service_using_SOAP_Request
               
                   <GetExportHarmonizeCountry xmlns=""http://tempuri.org/"">
                
-                     <Yearno>" + year + @"</Yearno>
+                     <Yearno>" + year.ToString() + @"</Yearno>
                
-                     <Monthno>" + month + @"</Monthno>
+                     <Monthno>" + month.ToString() + @"</Monthno>
                
                      <HarmonizeCode>" + harmonizeCode + @"</HarmonizeCode>
                
-                     <NoRank>" + rank + @"</NoRank>
+                     <NoRank>" + rank.ToString() + @"</NoRank>
                
                    </GetExportHarmonizeCountry>
                
@@ -149,9 +233,11 @@ namespace Calling_Web_Service_using_SOAP_Request
                     using (StreamReader rd = new StreamReader(Serviceres.GetResponseStream()))
                     {
                         //reading stream    
-                        var ServiceResult = rd.ReadToEnd();
-                        //writting stream result on console    
-                        Console.WriteLine(ServiceResult);
+                        var serviceResult = rd.ReadToEnd();
+
+                        XElement parsingServiceResult = XElement.Parse( serviceResult );
+                        //writting stream result on console
+                        Console.WriteLine( parsingServiceResult );
                         Console.WriteLine( "------------------------------------------------\n" );
                         //Console.ReadLine();
                     }
